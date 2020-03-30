62a5414bb22fd46041614465168cc46f573143a3
karifl <lampi_88@hotmail.com>
2020-03-30 23:47:59 +0300

    Update Brewer.java

--- singleton/Brewer.java
+++ singleton/Brewer.java
@@ -2,9 +2,10 @@ package singleton;
 
 public class Brewer {
 	public static void main(String[] args) {
-		//DrinkFactory pepsi = DrinkFactory.getInstance(new PepsiFactory());
+		DrinkFactory pepsi = DrinkFactory.getInstance(new PepsiFactory());
+		pepsi.listDrinks();
+		
 		DrinkFactory coca = DrinkFactory.getInstance(new CocaFactory());
-		//pepsi.listDrinks();
 		coca.listDrinks();
 	}
 }
